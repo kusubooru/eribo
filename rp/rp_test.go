d3403cb6fe5b00e352d4a4a9aec5c8f3f50d5737
Kusubooru <kusubooru@gmail.com>
2017-12-02 10:53:51 +0200

    Add poor, common, uncommon qualities for tktools

--- rp/rp_test.go
+++ rp/rp_test.go
@@ -13,6 +13,13 @@ func TestTieUps(t *testing.T) {
 	}
 }
 
+func checkEmoteTktool(t *testing.T, tool Tktool) {
+	t.Helper()
+	checkEmote(t, tool.Poor)
+	checkEmote(t, tool.Common)
+	checkEmote(t, tool.Uncommon)
+}
+
 func checkEmote(t *testing.T, s string) {
 	t.Helper()
 	if !strings.HasPrefix(s, "/me ") {
@@ -22,8 +29,22 @@ func checkEmote(t *testing.T, s string) {
 
 func checkBBcode(t *testing.T, s string) {
 	t.Helper()
-	if got, want := strings.Count(s, "[u]"), strings.Count(s, "[/u]"); got != want {
-		t.Errorf("Number of '[u]' = %d, number of '[/u]' = %d, message is: %q", got, want, s)
+	checkSyntax(t, s, "[", "]")
+	checkSyntax(t, s, "[u]", "[/u]")
+	checkSyntax(t, s, "[color=", "[/color]")
+}
+
+func checkBBcodeTktool(t *testing.T, tool Tktool) {
+	t.Helper()
+	checkBBcode(t, tool.Poor)
+	checkBBcode(t, tool.Common)
+	checkBBcode(t, tool.Uncommon)
+}
+
+func checkSyntax(t *testing.T, s, start, end string) {
+	t.Helper()
+	if got, want := strings.Count(s, start), strings.Count(s, end); got != want {
+		t.Errorf("Number of %q = %d, number of %q = %d, message is: %q", start, got, end, want, s)
 	}
 }
 
@@ -34,15 +55,32 @@ func checkVerbCount(t *testing.T, s, verb string, n int) {
 	}
 }
 
+func checkVerbCountTktool(t *testing.T, tool Tktool) {
+	t.Helper()
+	if tool.Colors != nil {
+		checkVerbCount(t, tool.Poor, "%s", 2)
+		checkVerbCount(t, tool.Common, "%s", 2)
+		checkVerbCount(t, tool.Uncommon, "%s", 2)
+	} else {
+		checkVerbCount(t, tool.Poor, "%s", 1)
+		checkVerbCount(t, tool.Common, "%s", 1)
+		checkVerbCount(t, tool.Uncommon, "%s", 1)
+	}
+}
+
+func checkQualityColorCode(t *testing.T, tool Tktool) {
+	t.Helper()
+	checkSyntax(t, tool.Poor, "[color=gray]", "[/color]")
+	checkSyntax(t, tool.Common, "[color=white]", "[/color]")
+	checkSyntax(t, tool.Uncommon, "[color=green]", "[/color]")
+}
+
 func TestTktools(t *testing.T) {
 	for _, tt := range tktools {
-		checkEmote(t, tt.Raw)
-		checkBBcode(t, tt.Raw)
-		if tt.Colors != nil {
-			checkVerbCount(t, tt.Raw, "%s", 2)
-		} else {
-			checkVerbCount(t, tt.Raw, "%s", 1)
-		}
+		checkEmoteTktool(t, tt)
+		checkBBcodeTktool(t, tt)
+		checkVerbCountTktool(t, tt)
+		checkQualityColorCode(t, tt)
 	}
 }
 
@@ -55,6 +93,52 @@ func TestVonproves(t *testing.T) {
 		}
 		if tt.HasUser {
 			checkVerbCount(t, tt.Raw, "%s", 1)
+		}
+	}
+}
+
+func TestTktool_Apply(t *testing.T) {
+	var tktoolApplyTests = []struct {
+		q    Quality
+		user string
+		tool Tktool
+		out  string
+	}{
+		{
+			q:    Common,
+			user: "Bob",
+			tool: Tktool{Common: "hand %s a common tool"},
+			out:  "hand Bob a common tool",
+		},
+		{
+			q:    Common,
+			user: "Bob",
+			tool: Tktool{Common: "hand %s a common %s tool", Colors: []Color{Red}},
+			out:  "hand Bob a common red tool",
+		},
+		{
+			q:    Poor,
+			user: "Bob",
+			tool: Tktool{Poor: "hand %s a poor tool", Common: "hand %s a common tool"},
+			out:  "hand Bob a poor tool",
+		},
+		{
+			q:    Unknown,
+			user: "Bob",
+			tool: Tktool{Poor: "hand %s a poor tool", Common: "hand %s a common tool"},
+			out:  "hand Bob a common tool",
+		},
+		{
+			q:    Uncommon,
+			user: "Bob",
+			tool: Tktool{Poor: "hand %s a poor tool", Common: "hand %s a common tool", Uncommon: "hand %s an uncommon tool"},
+			out:  "hand Bob an uncommon tool",
+		},
+	}
+	for _, tt := range tktoolApplyTests {
+		got, want := tt.tool.Apply(tt.user, tt.q), tt.out
+		if got != want {
+			t.Errorf("tktool.Apply(%q, %v) = %q, want %q", tt.user, tt.q, got, want)
 		}
 	}
 }
