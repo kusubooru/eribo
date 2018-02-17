309773f617d7f50b21071bdd49fcc17bf99cd766
Kusubooru <kusubooru@gmail.com>
2018-02-17 15:56:51 +0200

    Ping database first to make sure it is up
    Useful for server restarts.

--- eribo/mysql/mysql.go
+++ eribo/mysql/mysql.go
@@ -1,6 +1,9 @@
 package mysql
 
 import (
+	"log"
+	"time"
+
 	"github.com/jmoiron/sqlx"
 )
 
@@ -13,9 +16,23 @@ func NewEriboStore(dataSource string) (*EriboStore, error) {
 	if err != nil {
 		return nil, err
 	}
+	if err := pingDatabase(db); err != nil {
+		log.Fatalln("database ping attempts failed:", err)
+	}
 	store := &EriboStore{DB: db}
 	if err := store.createSchema(); err != nil {
 		return nil, err
 	}
 	return store, nil
+}
+
+func pingDatabase(db *sqlx.DB) (err error) {
+	for i := 0; i < 10; i++ {
+		err = db.Ping()
+		if err == nil {
+			return
+		}
+		time.Sleep(time.Second)
+	}
+	return
 }
